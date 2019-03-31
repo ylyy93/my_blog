a0d934bc6d09dabbd4389a74b8679314d47d4bb7
Jialuo Liu <lolofter@gmail.com>
2019-03-31 14:06:05 -0600

    rebuilding site Sun Mar 31 14:06:05 MDT 2019

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
-jekyll serve # if using a theme, replace with `hugo -t <YOURTHEME>`
+jekyll build
 
 # Add changes to git.
 git add .
